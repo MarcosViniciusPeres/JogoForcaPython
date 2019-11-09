c04d2f1c129338e1e2bc1a2b55fd83002ac470c5
Marcos <mvperes95@gmail.com>
2019-11-09 17:13:38 -0300

    Jogo da Forca Finalizado.

--- jogoForca/jogoForca.py
+++ jogoForca/jogoForca.py
@@ -4,65 +4,48 @@ vida = 5
 auxiliar = 0
 vetorVerdadeiro = []
 vetorAuxiliar = []
-
-def comecarJogo(self):
-    print('(1) - ESCOLHA O CONTEUDO DO JOGO DA FORCA')
-    print('(2) - SORTEAR UMA PALAVRA')
-    print('(3) - FALAR UMA LETRA')
-    print('(4) - TENTAR ACERTAR A PALAVRA INTEIRA')
-    print('(0) - SAIR DO PROGRAMA')
-
-    opcao = input('ESCOLHA UMA OPÇÃO: ')
-
-    if opcao == 1:
-        print("Voce apertou o 1")
-
-    elif opcao == 2:
-        print("Voce apertou o 2")
-
-    elif opcao == 3:
-        print("Voce apertou o 3")
-
-    elif opcao == 4:
-        print("Voce apertou o 4")
-
-    elif opcao == 0:
-        print("Voce apertou o 0")
+conteudo = []
+palavraSorteada = ""
+auxiliarEscolher = 0
 
 
 def escolherConteudo():
+    global auxiliarEscolher
+    if auxiliarEscolher == 0:
+        auxiliarEscolher += 1
+        variavelConteudo = int(input("Digite [1] para escolher Animal ou Digite [2] para escolher Fruta: "))
 
-    variavelConteudo = input("Digite [1] para escolher Animal ou Digite [2] para escolher Fruta: ")
-    if variavelConteudo == "1" or variavelConteudo == "2":
-        if variavelConteudo == "1":
-            print("Conteúdo Animal Foi Escolhido")
-            vetorConteudo = ["leao", "macaco", "elefante", "girafa", "zebra", "hiena", "suricato", "javali", "baleia", "sucuri", "borboleta", "onça", "abelha", "anta", "boi", "cabra", "camundongo", "cachorro", "gato", "esponja", "enguia", "flamingo", "formiga", "gafanhoto", "galinha", "gnu", "harpia", "hamster", "iguana", "jararaca", "jacare", "lacraia", "morcego", "ostra", "ovelha", "panda", "pernilongo", "porco", "rinoceronte", "salamandra", "tamandua", "tartaruga", "urso", "vaca", "veado"]
-            return vetorConteudo
-        if variavelConteudo == "2":
-            print("Conteúdo Fruta Foi Escolhido")
-            vetorConteudo = ["banana", "melancia", "laranja", "kiwi", "acerola", "abacate", "abacaxi", "Amora", "cacau", "pinha", "caju", "carambola", "jabuticaba", "figo", "framboesa", "goiaba", "groselha", "jaca", "limao", "mamao", "manga", "maracuja", "melao", "morango", "pera", "pessego", "pitanga", "pitaya", "roma", "tangerina", "uva"]
-            return vetorConteudo
+        if variavelConteudo == 1 or variavelConteudo == 2:
+            if variavelConteudo == 1:
+                print("Conteúdo Animal Foi Escolhido")
+                vetorConteudo = ["leao", "macaco", "elefante", "girafa", "zebra", "hiena", "suricato", "javali", "baleia", "sucuri", "borboleta", "onça", "abelha", "anta", "boi", "cabra", "camundongo", "cachorro", "gato", "esponja", "enguia", "flamingo", "formiga", "gafanhoto", "galinha", "gnu", "harpia", "hamster", "iguana", "jararaca", "jacare", "lacraia", "morcego", "ostra", "ovelha", "panda", "pernilongo", "porco", "rinoceronte", "salamandra", "tamandua", "tartaruga", "urso", "vaca", "veado"]
+                return vetorConteudo
+            if variavelConteudo == 2:
+                print("Conteúdo Fruta Foi Escolhido")
+                vetorConteudo = ["banana", "melancia", "laranja", "kiwi", "acerola", "abacate", "abacaxi", "Amora", "cacau", "pinha", "caju", "carambola", "jabuticaba", "figo", "framboesa", "goiaba", "groselha", "jaca", "limao", "mamao", "manga", "maracuja", "melao", "morango", "pera", "pessego", "pitanga", "pitaya", "roma", "tangerina", "uva"]
+                return vetorConteudo
+        else:
+            print("Escolha Uma Opção Proposta")
+            return
     else:
-        print("Escolha Uma Opção Proposta")
-        return None
+        print("Voce ja Escolheu um conteudo")
+        return
 
 def sortearPalavra(conteudo):
-
-    if len(conteudo) != 0:
-
+    if conteudo:
         numeroRandomico = randint(0, len(conteudo))
-
         palavraEscolhida = conteudo[numeroRandomico]
 
         print("Sua Palavra Foi Sorteada com Sucesso. Bora Jogar ?")
         return palavraEscolhida
     else:
         print("Escolha um conteudo Primeiramente antes de sortear uma palavra.")
-        return None
+        return
 
 
 
 def acertarPalavra(palavra):
+    global vida
     if vida != 0:
         if palavra:
             palavraDigitada = input("Digite a Palavra a Ser Advinhada Sem Acentuação: ")
@@ -72,7 +55,7 @@ def acertarPalavra(palavra):
                 print("Parabéns Você Venceu o Jogo Acertando a Palavra: ", palavra)
                 exit()
             else:
-                --vida
+                vida -= 1
                 print("Que pena Você Errou a Palavra a Ser Advinhada")
                 print("Agora Você Tem um Total de:", vida, "Vidas")
         else:
@@ -83,6 +66,8 @@ def acertarPalavra(palavra):
 
 
 def acertarLetra(palavra):
+    global auxiliar
+    global vida
     if vida != 0:
         if palavra:
             if vida == 0:
@@ -94,11 +79,11 @@ def acertarLetra(palavra):
             vetorChar = list(palavra)
 
             if auxiliar < len(vetorChar):
-               for i in range(auxiliar, len(vetorChar)):
+               for i in range(0, len(vetorChar)):
                    vetorVerdadeiro.append("_")
-                   ++auxiliar
+                   auxiliar += 1
 
-            print("Você tem: ",vida," Vidas")
+            print("Você tem: ", vida, " Vidas")
             letraChar = input("Escolha Uma Letra A Ser Adivinhada: ")
 
             letraChar.lower()
@@ -114,7 +99,7 @@ def acertarLetra(palavra):
             for i in range(len(vetorChar)):
                 if letraChar == vetorChar[i]:
                     vetorVerdadeiro[i] = letraChar
-                    ++contador
+                    contador += 1
                     vetorAuxiliar.append(letraChar)
 
             for i in range(len(vetorChar)):
@@ -128,7 +113,7 @@ def acertarLetra(palavra):
                 exit()
 
             if contador == 0:
-                --vida
+                vida -= 1
                 print("Que Pena Nao Existe Essa Letra na Palavra.")
                 print("Agora Você Tem um Total de: ", vida, " Vidas")
 
@@ -140,7 +125,46 @@ def acertarLetra(palavra):
             print("Palavra: ")
             for i in range(len(vetorVerdadeiro)):
                 print(vetorVerdadeiro[i], end=" ")
+            print("")
         else:
             print("Sorteie uma Palavra Primeiramente para tentar Acertar A Letra")
+            return
     else:
         print("Que pena suas Vidas Acabaram!! Recomece outro Jogo.")
+        exit()
+
+
+def menuInicial():
+    print('(1) - ESCOLHA O CONTEUDO DO JOGO DA FORCA')
+    print('(2) - SORTEAR UMA PALAVRA')
+    print('(3) - FALAR UMA LETRA')
+    print('(4) - TENTAR ACERTAR A PALAVRA INTEIRA')
+    print('(0) - SAIR DO PROGRAMA')
+
+
+def comecaJogo():
+    global conteudo
+    global palavra
+    global palavraSorteada
+
+    opcao = 1
+    while opcao != 0:
+        menuInicial()
+        opcao = int(input('ESCOLHA UMA OPÇÃO: '))
+
+        if opcao == 0 or opcao == 1 or opcao == 2 or opcao == 3 or opcao == 4:
+            if opcao == 1:
+                conteudo = escolherConteudo()
+            elif opcao == 2:
+                palavraSorteada = sortearPalavra(conteudo)
+            elif opcao == 3:
+                acertarLetra(palavraSorteada)
+            elif opcao == 4:
+                acertarPalavra(palavraSorteada)
+            elif opcao == 0:
+                print("Saindo do Programa")
+                exit()
+        else:
+            print("ERRO: Opção inválida.")
+
+comecaJogo()
